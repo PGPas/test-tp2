f58949c91b9ec369ba7510426197e5299f38f903
PG_Pas <pascal.pierregabriel@gmail.com>
2018-01-27 21:45:28 +0100

    Put tests written

--- test/unit/repository/UserRepositoryTest.js
+++ test/unit/repository/UserRepositoryTest.js
@@ -89,5 +89,40 @@ describe("UserRepository", function() {
         var repository = new UserRepository(mockDb);
         expect(repository.findOneById('-1234567')).toEqual(null);
     });
+
+    describe("UserRepository", function() {
+
+    /*** Put ***/
+
+    it("should call db.write", function(){
+        var mockDb = jasmine.createSpyObj('db', ['get', 'push', 'write']);
+        mockDb.get.and.returnValue(mockDb);
+        mockDb.push.and.returnValue(mockDb);
+
+        var repository = new UserRepository(mockDb);
+        repository.update({
+            id : 1,
+            firstname: 'John',
+            lastname : 'Doe',
+            birthday : '2000-01-01'
+        });
+
+        expect(mockDb.push).toHaveBeenCalledWith({
+            id : 1,
+            firstname: 'John',
+            lastname : 'Doe',
+            birthday : '2000-01-01'
+        });
+        expect(mockDb.write).toHaveBeenCalledTimes(1);
+    });
+
+    it("should throw exception undefined", function(){
+        var repository = new UserRepository({});
+        var f = function(){
+            repository.update();
+        };
+
+        expect(f).toThrow('User object is undefined')
+    });
     
 });
